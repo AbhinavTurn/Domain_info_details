acc7b605e7fd9a77a1447d62ec8806e2f020b773
Abhinav <abhinavra927@gmail.com>
2025-04-03 20:16:51 +0530

    Create api_keys.py

---
+++ api_keys.py
@@ -0,0 +1,6 @@
+# Store API keys here
+SHODAN_API_KEY = "your shodan api key"
+HIBP_API_KEY = "your_haveibeenpwned_api_key"
+VIRUSTOTAL_API_KEY = "your api key"
+CENSYS_API_ID = "your_censys_api_id"
+CENSYS_SECRET = "your_censys_secret"
